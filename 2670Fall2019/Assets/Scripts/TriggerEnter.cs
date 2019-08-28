3ed96c0d33435c654fc57e3b786b8ddde516c6f3
Grace <heya21st@gmail.com>
2019-08-28 13:57:19 -0600

    Committed

---
+++ 2670Fall2019/Assets/Scripts/TriggerEnter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEnter : MonoBehaviour
+{
+    public UnityEvent TriggerEnterEvent, TriggerStayEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggerEnterEvent.Invoke(); 
+    }
+
+    private void ONTriggerStay(Collider other)
+    {
+        TriggerStayEvent.Invoke();
+    }
+    
+}
