25768f052ae5d7926f8d4a8ed02db61d2db69da3
Grace <heya21st@gmail.com>
2019-09-10 23:39:24 -0600

    Committed

--- 2670Fall2019/Assets/Scripts/GameAction.cs
+++ 2670Fall2019/Assets/Scripts/GameAction.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 [CreateAssetMenu]
-public class GameAction : MonoBehaviour
+public class GameAction : ScriptableObject
 {
     public UnityAction action;
 
