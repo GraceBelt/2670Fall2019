ad7f5673d81073d46114075e44bb9c7b17665fdc
Grace <heya21st@gmail.com>
2019-09-18 13:45:22 -0600

    Committed

---
+++ 2670Fall2019/Assets/Scripts/ActionCalls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionCalls : MonoBehaviour
+{
+    public float speed = 1f, rotateSpeed = 10f, scale Speed = 0.2
+
+    public void Update()
+    {
+        OnMove();
+        OnRotate();
+    }
+
+    public void OnMove()
+    {
+        location.x = speed * Time.deltaTime;
+        transform.Rotate(location);
+    }
+
+    public void OnRotate()
+    {
+        y = rotateSpeed * Time.deltaTime;
+        transform.Rotate(rotations);
+    }
+
+    public void OnScale()
+    {
+        scales.Set()
+    }
+
+}
