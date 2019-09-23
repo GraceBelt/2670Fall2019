06c6e8bf00438b14e8b0c44a627d0beccb63870f
Grace <heya21st@gmail.com>
2019-09-22 22:40:41 -0600

    Committed

--- 2670Fall2019/Assets/Scripts/TriggerEventBehaviour.cs
+++ 2670Fall2019/Assets/Scripts/TriggerEventBehaviour.cs
@@ -5,6 +5,8 @@ using UnityEngine.Events;
 
 public class TriggerEventBehaviour : MonoBehaviour
 {
+    //What is OnTriggerEnter? It is an object to enter the other object that will trigger a sign. 
+    //Remember, the Collider Objects do need Rigidbody. 
     public UnityEvent TriggerEnterEvent;
 
     private void OnTriggerEnter(Collider other)
