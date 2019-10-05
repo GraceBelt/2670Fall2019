bd7000d0e31862e39529978b8e359e0b22b16a4b
Grace <heya21st@gmail.com>
2019-10-04 23:22:13 -0600

    Committed

--- 2670Fall2019/Assets/All/Scripts/TriggerEventBehaviour.cs
+++ 2670Fall2019/Assets/All/Scripts/TriggerEventBehaviour.cs
@@ -5,13 +5,13 @@ public class TriggerEventBehaviour : MonoBehaviour
 {
     public UnityEvent TriggerEnterEvent, TriggerExitEvent;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)//OnTriggerEnter is a trigger when player enter the collider. 
     {
         TriggerEnterEvent.Invoke();
         print("Enter");
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider other)//OnTriggerExit is a trigger when player exit the collider. 
     {
         TriggerExitEvent.Invoke();
         print("Exit");
