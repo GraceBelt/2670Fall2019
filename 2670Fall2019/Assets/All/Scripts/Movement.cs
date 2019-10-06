658346072070d0ad38a33392f87fe407231925ba
Grace <heya21st@gmail.com>
2019-10-05 22:37:41 -0600

    Committed

---
+++ 2670Fall2019/Assets/All/Scripts/Movement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Movement : MonoBehaviour
+{
+    public Rigidbody Player;
+    public float speed = 5f;
+    void Start()
+    {
+        
+    }
+}
