bbf7e45b24627f050e694f1243b61f6b931d62fc
Grace <heya21st@gmail.com>
2019-10-15 23:37:56 -0600

    Committed

---
+++ 2670Fall2019/Assets/All/Scripts/MouseEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UnityEvent MouseDownEvent;
+    private void OnMouseDown()
+    {
+        MouseDownEvent.Invoke();
+    }
+}
+
