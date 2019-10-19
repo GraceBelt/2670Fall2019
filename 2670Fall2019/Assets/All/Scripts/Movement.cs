68609005f93316ea759c89f41fe7701f40b7ecb3
Grace <heya21st@gmail.com>
2019-10-18 23:44:41 -0600

    Committed

--- 2670Fall2019/Assets/All/Scripts/Movement.cs
+++ 2670Fall2019/Assets/All/Scripts/Movement.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(CharacterController))]
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 10f;
+    private CharacterController controller;
+    private Vector3 position;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        position.x = moveSpeed * Input.GetAxis("Horizontal");
+        controller.Move(position * Time.deltaTime);
     }
 }
