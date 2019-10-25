6e5f10ef1c5c6cf86c02aab51de486ad1d93af8a
Grace <heya21st@gmail.com>
2019-10-24 23:41:18 -0600

    Committed

--- 2670Fall2019/Assets/All/Scripts/MyScripts/Death.cs
+++ 2670Fall2019/Assets/All/Scripts/MyScripts/Death.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using UnityEngine.XR.WSA.Input;
 
 public class Death : MonoBehaviour
 {
     public UnityEvent TriggerEnter;
     public Text GameOver;
-    private Coroutines waitForSeconds;
+    public bool gameOver;
 
     private void OnTriggerEnter(Collider other)
     {
         TriggerEnter.Invoke();
         Destroy(other.gameObject);
     }
-    
-    
+
+    private void EndGame()
+    {
+        if (gameOver)
+        {
+            GameOver.text = "";
+            gameOver = true;
+        }
+    }
 }
