6819de255db6165d794c12fc2b8e9cebcd35e1a5
Grace <heya21st@gmail.com>
2019-11-13 16:01:31 -0700

    30 Minutes Committed

---
+++ 2670Fall2019/Assets/All/Scripts/MyScripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    private CharacterController controller;
+    private Vector3 position;
+    
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        position.z = moveSpeed * Input.GetAxis("Horizontal");
+        controller.Move(position * Time.deltaTime);
+    }
+}
