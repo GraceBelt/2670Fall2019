d9c1d4a145bf55fb65ac377111d52399e5ca1a20
Grace <heya21st@gmail.com>
2019-11-21 23:15:29 -0700

    Committed

---
+++ 2670Fall2019/Assets/All/Scripts/MyScripts/PowerUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerUp : MonoBehaviour
+{
+    public Text health;
+    public int Greater = 1;
+    public CharacterController Player;
+
+    private void Text()
+    {
+        health.text = "Health: " + Greater;
+    }
+    
+    
+    private void OnTriggerEnter(Collider Player)
+    {
+        Greater++;
+        Text();
+    }
+}
